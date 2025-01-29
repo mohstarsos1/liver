df7b154d6a899daa515ec06d48758574148a6ca7
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2025-01-29 18:38:12 +0330

    add 404 page

--- server.js
+++ server.js
@@ -14,7 +14,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 
 app.get("/", (req, res) => {
-    res.render("pages/home", { title: "main" });
+    res.render("pages/home", { title: ".: Liver :.", pageTitle: "داشبورد" });
+});
+
+app.get("/about", (req, res) => {
+    res.render("pages/about", { title: ".: About Us :.", pageTitle: "درباره ما" });
+});
+
+app.get("/login", (req, res) => {
+    res.render("pages/login", { title: ".: Login :.", layout: "layouts/auth-layout" });
+});
+
+app.get("/register", (req, res) => {
+    res.render("pages/register", { title: ".: Register :.", layout: "layouts/auth-layout" });
+});
+
+app.get("*", function (req, res) {
+    res.status(404).render("pages/404", { title: ".: Error 404 :.", layout: "layouts/auth-layout" });
 });
 
 app.listen(5000, () => {
